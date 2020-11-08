156ce8f97de3a74b9303aabdf07a0f41f37900ba
wkgcass <wkgcass@hotmail.com>
2020-11-09 00:13:22 +0800

    [noto-font plugin] implement the plugin

---
+++ plugins/noto-font/README.md
@@ -0,0 +1,7 @@
+# Noto字体
+
+## 构建
+
+需要将[这里](https://github.com/wkgcass/Chara-bundle/tree/main/font/noto)需要的字体文件放置在该工程的`plugin/noto-font/font/`目录中`
+
+然后执行`./build-plugin.sh`
--- plugins/noto-font/src/main/java/net/cassite/desktop/chara/plugin/notofont/NotoFontPlugin.java
+++ plugins/noto-font/src/main/java/net/cassite/desktop/chara/plugin/notofont/NotoFontPlugin.java
@@ -2,11 +2,15 @@
 
 package net.cassite.desktop.chara.plugin.notofont;
 
+import javafx.scene.text.Font;
+import net.cassite.desktop.chara.i18n.Words;
+import net.cassite.desktop.chara.i18n.WordsBuilder;
+import net.cassite.desktop.chara.manager.FontManager;
 import net.cassite.desktop.chara.plugin.Plugin;
 import net.cassite.desktop.chara.util.Logger;
 import net.cassite.desktop.chara.util.ResourceHandler;
 
-import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class NotoFontPlugin implements Plugin {
@@ -24,30 +28,57 @@ public class NotoFontPlugin implements Plugin {
     }
 
     @Override
+    public double priority() {
+        return GENERAL_FONT_PRIORITY;
+    }
+
+    @Override
     public List<ResourceHandler> resourceHandlers() {
-        return Collections.emptyList();
+        List<ResourceHandler> ret = new LinkedList<>();
+        Words words = new WordsBuilder
+            ("zh")
+            .setEn("en")
+            .build();
+        String lang = words.get()[0];
+        //noinspection SwitchStatementWithTooFewBranches
+        switch (lang) {
+            case "zh":
+                ret.add(new ResourceHandler("font/NotoSansSC-Regular.otf", (inputStream, cb) -> {
+                    FontManager.registerFont(inputStream);
+                    FontManager.setDefaultFontFamily("Noto Sans SC Regular");
+                    FontManager.setDefaultMonospaceFontFamily("Noto Mono for Powerline");
+                    cb.succeeded(null);
+                }));
+                break;
+            default:
+                Logger.error("The plugin does not contain your language: " + lang);
+        }
+        return ret;
     }
 
     @Override
     public void launch() {
-        Logger.info(name() + " launch()");
+        // do nothing
+        Logger.info("current font families list: " + Font.getFamilies());
     }
 
     @Override
     public void clicked() {
-        Logger.info(name() + " clicked()");
+        // do nothing
     }
 
     @Override
     public void release() {
-        Logger.info(name() + " release()");
+        // do nothing
     }
 
     @Override
     public String about() {
         return "" +
-            "author: $AUTHOR\n" +
-            "code license: GPLv2 with classpath exception" +
+            "author: wkgcass\n" +
+            "code license: GPLv2 with classpath exception\n" +
+            "font: noto\n" +
+            "font license: Open Font License" +
             "";
     }
 }
